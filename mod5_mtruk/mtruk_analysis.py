f23457dc5e4d687b56c1909ed9d4f17bec321bea
“Chandan <chandan77m@hotmail.com>
2026-03-15 19:12:49 -0400

    new cgs

--- mod5_mtruk/mtruk_analysis.py
+++ mod5_mtruk/mtruk_analysis.py
@@ -12,3 +12,10 @@ proportion_accepted = data['Y'].mean()
 print(f"Proportion of observations that accepted the coupon: {proportion_accepted:.2f}")
 
 # new changes
+
+proportion_accepted_bar_coupons = data[data['coupon'] == 'Bar']['Y'].value_counts(normalize=True).get(1, 0)
+print(f"Proportion of bar coupons accepted: {proportion_accepted_bar_coupons:.2f}")
+
+portion_bar_acccepsted = data.query('coupon == "Bar" & Y == 1')
+fraction_of_tot_bar =  portion_bar_acccepsted.shape[0]/ data.query('coupon == "Bar"').shape[0]
+print(f"Interms of percentage of bar coupons accepted: {fraction_of_tot_bar:.2%}")
