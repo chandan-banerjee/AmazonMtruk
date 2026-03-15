a4ea07424cb53d61091a14a0bd5f97fb1d3da27f
chandan-banerjee <70322760+chandan-banerjee@users.noreply.github.com>
2026-03-15 15:59:36 -0400

    Add files via upload

---
+++ mtruk_analysis.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('/Users/nandikabanerjee/AI/AmazonMtruk/mod5_mtruk/assignment5_1_starter/data/coupons.csv')
+
+print(data.head())
+
+# What proportion of the total observations chose to accept the coupon?
+proportion_accepted = data['Y'].mean()
+print(f"Proportion of observations that accepted the coupon: {proportion_accepted:.2f}")
