3b27681b20dda82c04bd3c0d088d608bd2bad219
“Chandan <chandan77m@hotmail.com>
2026-03-16 00:56:17 -0400

    bar calculation

--- SelfSudy_Python/mtruk_analysis.py
+++ SelfSudy_Python/mtruk_analysis.py
@@ -9,13 +9,16 @@ print(data.head())
 
 # What proportion of the total observations chose to accept the coupon?
 proportion_accepted = data['Y'].mean()
-print(f"Proportion of observations that accepted the coupon: {proportion_accepted:.2f}")
+print(f"Overall coupon acceptance rate: {proportion_accepted:.2f}")
 
-# new changes
+# What proportion of bar coupons were accepted?
 
 proportion_accepted_bar_coupons = data[data['coupon'] == 'Bar']['Y'].value_counts(normalize=True).get(1, 0)
-print(f"Proportion of bar coupons accepted: {proportion_accepted_bar_coupons:.2f}")
+print(f"\n Proportion of bar coupons accepted: {proportion_accepted_bar_coupons:.2f}")
 
 portion_bar_acccepsted = data.query('coupon == "Bar" & Y == 1')
 fraction_of_tot_bar =  portion_bar_acccepsted.shape[0]/ data.query('coupon == "Bar"').shape[0]
-print(f"Interms of percentage of bar coupons accepted: {fraction_of_tot_bar:.2%}")
+print(f"\nInterms of percentage of bar coupons accepted: {fraction_of_tot_bar:.2%}")
+
+
+# Compare the acceptance rate between those who went to a bar 3 or fewer times a month to those who went more.
